9787ca943184ccf7c567918bcb1a05f827185b61
Kim EunSil <kim.eunsil@hardcon.co.kr>
2023-10-16 19:06:52 +0900

    수정합니다. 커밋

--- WindowsFormsApp1/WindowsFormsApp1/Form1.cs
+++ WindowsFormsApp1/WindowsFormsApp1/Form1.cs
@@ -19,7 +19,7 @@ namespace WindowsFormsApp1
 
         private void button1_Click(object sender, EventArgs e)
         {
-            MessageBox.Show("git hub test ");
+            MessageBox.Show("git hub test 수정합니다.  ");
 
         }
     }
